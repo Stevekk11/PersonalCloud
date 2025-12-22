7c7f7a3921ce5bfd5e4c8e96fa32a03051dc8cb3
Stevek <vegh@spsejecna.cz>
2025-12-22 17:01:19 +0100

    rm

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,7 +13,6 @@ public class HomeController : Controller
     private readonly UserManager<ApplicationUser> _userManager;
     private readonly SignInManager<ApplicationUser> _signInManager;
     private readonly DocumentService _documentService;
-    private readonly Helpers.Dht22SerialService _dht22Service;
 
     public HomeController(ILogger<HomeController> logger, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, DocumentService documentService)
     {
@@ -21,28 +20,13 @@ public class HomeController : Controller
         _userManager = userManager;
         _signInManager = signInManager;
         _documentService = documentService;
-        _dht22Service = new Helpers.Dht22SerialService(); // Default COM3, 9600
     }
 
     public async Task<IActionResult> Index(string? comPort = null, bool retry = false)
     {
-        if (comPort != null)
-        {
-            _dht22Service.ChangePort(comPort);
-        }
-        if (retry)
-        {
-            // Force a retry by closing and reopening the port
-            _dht22Service.ChangePort(_dht22Service.PortName);
-        }
         var model = new HomeDashboardViewModel
         {
             ServerTimeUtc = DateTime.UtcNow,
-            ServerTemperature = _dht22Service.GetTemperature(),
-            ServerHumidity = _dht22Service.GetHumidity(),
-            SensorError = _dht22Service.LastError,
-            SelectedComPort = _dht22Service.PortName,
-            AvailableComPorts = SerialPort.GetPortNames().ToList()
         };
 
         if (User.Identity?.IsAuthenticated == true)
@@ -76,19 +60,6 @@ public class HomeController : Controller
         return View();
     }
 
-    [HttpPost]
-    public IActionResult ChangeComPort(string comPort)
-    {
-        _dht22Service.ChangePort(comPort);
-        return RedirectToAction("Index", new { comPort });
-    }
-
-    [HttpPost]
-    public IActionResult RetrySensor(string comPort)
-    {
-        _dht22Service.ChangePort(comPort);
-        return RedirectToAction("Index", new { comPort, retry = true });
-    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
--- Models/HomeDashboardViewModel.cs
+++ Models/HomeDashboardViewModel.cs
@@ -17,10 +17,4 @@ public class HomeDashboardViewModel
     public string AccountTypeLabel => IsPremium ? "Premium" : "Free";
 
     public DateTime ServerTimeUtc { get; set; }
-
-    public double? ServerTemperature { get; set; }
-    public double? ServerHumidity { get; set; }
-    public string? SensorError { get; set; }
-    public string? SelectedComPort { get; set; }
-    public List<string> AvailableComPorts { get; set; } = new();
 }
