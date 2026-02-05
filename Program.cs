40d9df6277e0b0e38e60313c3cda290c5ea7aa16
Stevek <vegh@spsejecna.cz>
2026-02-05 16:20:35 +0100

    Add missing localization

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Http.Features;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
