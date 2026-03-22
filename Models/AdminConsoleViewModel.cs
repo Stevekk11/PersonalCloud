722981c26a21b79efbd68d9c663c1c2c67a6bb2c
Stevek <vegh@spsejecna.cz>
2026-03-22 10:59:58 +0100

    add console

---
+++ Models/AdminConsoleViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace PersonalCloud.Models;
+
+public class AdminConsoleViewModel
+{
+    public string? Password { get; set; }
+    public bool IsAuthenticated { get; set; }
+    public string? Command { get; set; }
+    public string? Output { get; set; }
+    public string? ErrorMessage { get; set; }
+}
