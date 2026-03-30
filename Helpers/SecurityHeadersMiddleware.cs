e9389350fe40a7cd0ba53768158b9e6ec84afa54
Stevek <vegh@spsejecna.cz>
2026-03-30 16:48:09 +0200

    Fixes

--- Helpers/SecurityHeadersMiddleware.cs
+++ Helpers/SecurityHeadersMiddleware.cs
@@ -16,14 +16,15 @@
             context.Response.Headers.Append("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
 
             // Content-Security-Policy
-            var csp = "default-src 'self'; " +
-                      "script-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net https://challenges.cloudflare.com/turnstile/v0/api.js;" +
-                      "object-src 'none'; " +
-                      "style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; " +
-                      "img-src 'self' data:; " +
-                      "font-src 'self' https://cdn.jsdelivr.net; " +
-                      "connect-src 'self'; " +
-                      "frame-ancestors 'self'; " +
+            var csp = "default-src 'self' https://challenges.cloudflare.com https://pid.cz https://data.pid.cz;" +
+                      "script-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net https://challenges.cloudflare.com;" +
+                      "style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net;" +
+                      "img-src 'self' data:;" +
+                      "font-src 'self' https://cdn.jsdelivr.net;" +
+                      "connect-src 'self' https://challenges.cloudflare.com;" +
+                      "frame-src https://challenges.cloudflare.com https://pid.cz https://data.pid.cz;" +
+                      "object-src 'none';" +
+                      "frame-ancestors 'self';" +
                       "form-action 'self';";
 
             context.Response.Headers.Append("Content-Security-Policy", csp);
@@ -50,4 +51,3 @@
         }
     }
 }
-
